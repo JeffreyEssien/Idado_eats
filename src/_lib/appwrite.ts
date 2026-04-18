b3329bc396b25ae779cc9390bb4f9f733ff6041b
JeffreyEssien <adizastone89@gmail.com>
2026-04-18 11:29:30 +0100

    first push

--- src/_lib/appwrite.ts
+++ src/_lib/appwrite.ts
@@ -1,9 +1,28 @@
 import { Client, Account, Databases } from 'appwrite'
 
-const client = new Client()
-  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!)
+let _client: Client | null = null
+let _account: Account | null = null
+let _databases: Databases | null = null
 
-export const account = new Account(client)
-export const databases = new Databases(client)
-export { client }
+function getClient() {
+  if (!_client) {
+    _client = new Client()
+      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!)
+  }
+  return _client
+}
+
+export const account = new Proxy({} as Account, {
+  get(_, prop) {
+    if (!_account) _account = new Account(getClient())
+    return (_account as unknown as Record<string | symbol, unknown>)[prop]
+  },
+})
+
+export const databases = new Proxy({} as Databases, {
+  get(_, prop) {
+    if (!_databases) _databases = new Databases(getClient())
+    return (_databases as unknown as Record<string | symbol, unknown>)[prop]
+  },
+})
